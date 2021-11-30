32b73b6bdc3d01b38b300b6278182f21e9c77f83
Rizky Dwi saputra <rizkydwisaputrar1@gmail.com>
2021-11-30 07:28:46 -0800

    p

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask 
+from flask_mysqldb import MySQL 
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'big_project'
-
-from application import route
+app.config.update(dict(
+  SECRET_KEY="powerful secretkey",
+  WTF_CSRF_SECRET_KEY="a csrf secret key"
+))
+mysql = MySQL(app)
+from application import routes
--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,9 @@
-from flask import render_template, request, url_for, redirect
-from application import app
-from flask_mysqldb import MySQL
-mysql = MySQL(app)
+from enum import unique
+from flask.wrappers import Response
+from flask import json, render_template, request,Response, url_for, redirect
+from application import app , mysql
+from application.forms import LoginForm
+from application.forms import LoginForm, RegisterForm
 
 @app.route('/')
 @app.route('/home')
@@ -11,19 +13,31 @@ def index():
     cur.execute("SELECT * FROM mobil")
     rv = cur.fetchall()
     cur.close() 
-    render_template('index.html', mobil=rv)
-    
-@app.route('/admin')
+    return render_template('index.html', mobil=rv)
+@app.route('/login',methods=['GET','POST'])
+def login():
+    form =LoginForm()
+    if request.method == "POST":
+        details = request.form
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT username FROM admin")
+        nama = cur.fetchall() 
+        cur.execute("SELECT password FROM admin")
+        pswd = cur.fetchall()
+        cur.close()
+        return 
+    return render_template("login.html", title="Login",form =form, login=True)
+@app.route('/warga')
+def warga():
+    warga = mysql.connection.cursor()
+    warga.execute("SELECT * FROM data_warga")
+    data_warga = warga.fetchall()
+    warga.close()
+    return render_template('warga.html', data_warga=data_warga)
+@app.route('/login/admin')
 def admin():
     kabel = mysql.connection.cursor()
-    kabel.execute("SELECT * FROM admin")
+    kabel.execute("SELECT * FROM qna")
     tabel = kabel.fetchall()
     kabel.close()
-    return render_template('login.html', admin=tabel)
-@app.route('/warga')
-def admin():
-    warga = mysql.connection.cursor()
-    warga.execute("SELECT * FROM warga")
-    data_warga = warga.fetchall()
-    data_warga.close()
-    return render_template('warga.html', data_warga=data_warga)
+    return render_template('/admin/index.html', admin=tabel)
