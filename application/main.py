b91a069abdf4931f4d2ab5c5c59ddfcd727bafea
Rizky Dwi Saputra <55983435+rixkyduwi@users.noreply.github.com>
2022-03-21 14:44:29 +0700

    summary

--- application/main.py
+++ application/main.py
@@ -11,9 +11,7 @@ import datetime
 import nltk,pickle,json,random;nltk.download('popular')
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
-import numpy as np
-from keras.models import load_model
-model = load_model('model.h5')
+import numpy as np 
 intents = json.loads(open('application/content.json').read())
 words = pickle.  load(open('texts.pkl','rb'))
 classes = pickle.load(open('labels.pkl','rb'))
@@ -211,70 +209,6 @@ def deleteb3(id):
     except Exception as e:
         return make_response(e)
     return redirect(url_for('main.b3'))
-@main.route('/admin/latih_chatbot',methods=['GET','POST'])
-@login_required
-def latih_chatbot():
-    return jsonify(intents)
-@main.route('/admin/listadmin')
-@login_required
-def listadmin():
-    return render_template('admin/listadmin.html', admin = User.query.all().fetchall(),listadmin=True)
-
-@main.route('/chatbot')
-def chatbot():
-    return render_template('admin/chatbot.html')
-def clean_up_sentence(sentence):
-    # tokenize the pattern - split words into array
-    sentence_words = nltk.word_tokenize(sentence)
-    # stem each word - create short form for word
-    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
-    return sentence_words
-# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
-
-def bow(sentence, words, show_details=True):
-    # tokenize the pattern
-    sentence_words = clean_up_sentence(sentence)
-    # bag of words - matrix of N words, vocabulary matrix
-    bag = [0]*len(words)  
-    for s in sentence_words:
-        for i,w in enumerate(words):
-            if w == s: 
-                # assign 1 if current word is in the vocabulary position
-                bag[i] = 1
-                if show_details:
-                    print ("found in bag: %s" % w)
-    return(np.array(bag))
-
-def predict_class(sentence, model):
-    # filter out predictions below a threshold
-    p = bow(sentence, words,show_details=False)
-    res = model.predict(np.array([p]))[0]
-    ERROR_THRESHOLD = 0.25
-    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
-    # sort by strength of probability
-    results.sort(key=lambda x: x[1], reverse=True)
-    return_list = []
-    for r in results:
-        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
-    return return_list
-
-def getResponse(ints, intents_json):
-    tag = ints[0]['intent']
-    list_of_intents = intents_json['intents']
-    for i in list_of_intents:
-        if(i['tag']== tag):
-            result = random.choice(i['responses'])
-            break
-    return result
-
-def chatbot_response(msg):
-    ints = predict_class(msg, model)
-    res = getResponse(ints, intents)
-    return res 
-@main.route("/get")
-def get_bot_response():
-    userText = request.args.get('msg')
-    return chatbot_response(userText)
 @main.route('/history/<username>', methods=['GET'])
 def historyuser_post(username):
     warga = mysql.connection.cursor()
