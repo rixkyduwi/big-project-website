824f7b9f8d4b642bcabe0c017af580c2a74c4d30
Rizky Dwi Saputra <55983435+rixkyduwi@users.noreply.github.com>
2022-03-22 13:16:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from application import app
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL 
+# init SQLAlchemy so we can use it later in our models
+db = SQLAlchemy()
+mysql = MySQL()
+UPLOAD_FOLDER = 'application/static/upload/'
+main = Flask(__name__)
+@main.route('/')
+def index():
+    return "halloworld"
+    
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
