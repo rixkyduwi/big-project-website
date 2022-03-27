1571c10fc2ec98ea65f11f8887d8391866ee532c
Rizky Dwi Saputra <55983435+rixkyduwi@users.noreply.github.com>
2022-03-28 00:40:15 +0700

    summary

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app.config['MYSQL_DATABASE_USER'] = 'bccc1e5d68a972'
 app.config['MYSQL_DATABASE_PASSWORD']  = '1171dc99'
 app.config['MYSQL_DATABASE_DB'] = 'heroku_068afdbbc88db22'
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(port=5000, debug=True)
